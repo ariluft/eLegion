b535b3775d752351593831201b4cbe237c6d2e34
Petr Kononov <ariluft@gmail.com>
2020-03-27 22:17:57 +0300

    fix preferences

--- app/src/main/java/com/pkononov/elegion/SharedPreferencesHelper.kt
+++ app/src/main/java/com/pkononov/elegion/SharedPreferencesHelper.kt
@@ -4,36 +4,37 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 import java.util.*
 import kotlin.collections.ArrayList
 
-class SharedPreferencesHelper(context:Context) {
+class SharedPreferencesHelper(context: Context) {
 
     private var mSharedPreferences: SharedPreferences
     private val mGson = Gson()
 
-    init{
+    init {
         mSharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
     }
 
-    companion object{
+    companion object {
         const val SHARED_PREF_NAME = "SHARED_PREF_NAME"
         const val USERS_KEY = "USERS_KEY"
-        val USERS_TYPE = object : TypeToken<List<User>>() {}.type
+        val USERS_TYPE: Type = object : TypeToken<List<User>>() {}.type
     }
 
-    fun getUsers():ArrayList<User>{
-        val users:ArrayList<User> =
-            mGson.fromJson(mSharedPreferences.getString(USERS_KEY, USERS_KEY), USERS_TYPE)
-                ?: return ArrayList<User>()
+    private fun getUsers(): ArrayList<User> {
+        val users: ArrayList<User> =
+            mGson.fromJson(mSharedPreferences.getString(USERS_KEY, ""), USERS_TYPE)
+                ?: return ArrayList()
         return users
     }
 
-    fun addUser(user:User):Boolean{
+    fun addUser(user: User): Boolean {
         val users = getUsers()
         users.forEach {
-        if (it.login.toLowerCase(Locale.ROOT) == user.login.toLowerCase(Locale.ROOT)) {
-            return false
+            if (it.login.toLowerCase(Locale.ROOT) == user.login.toLowerCase(Locale.ROOT)) {
+                return false
             }
         }
 
--- app/src/main/java/com/pkononov/elegion/User.kt
+++ app/src/main/java/com/pkononov/elegion/User.kt
@@ -2,4 +2,14 @@ package com.pkononov.elegion
 
 import java.io.Serializable
 
-class User(var login:String, var password:String) : Serializable
+class User(var login:String, var password:String) : Serializable{
+    private var mHasSuccessLogin = false
+
+    fun hasSuccessLogin() : Boolean{
+        return mHasSuccessLogin
+    }
+
+    fun setHasSuccessLogin(){
+        mHasSuccessLogin = true
+    }
+}
